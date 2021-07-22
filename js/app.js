4c24384508b1f67fe1621c52cbc772ba11f8816a
payoneercashout <56314279+payoneercashout@users.noreply.github.com>
2021-07-22 20:56:39 +0300

    scrol

--- js/app.js
+++ js/app.js
@@ -122,5 +122,22 @@ $(function () {
 
         nav.toggleClass("show");
     });
+
+    /*Smooth scrol*/
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    /*Smooth scrol*/
     
 });
